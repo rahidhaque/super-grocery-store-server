6dfa68c47c693327f6c4ac2fae5fa23cf9bfb196
rahidhaque <ontirahid@yahoo.com>
2022-05-04 09:30:59 +0600

    Database Connection Setup Done

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhriv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("Warehouse Server is running");
+})
+
+app.listen(port, () => {
+    console.log("Listening to Port: ", port);
+})
--- node.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-const port = process.env.PORT || 5000;
-const app = express();
-
-//middleware
-app.use(cors());
-app.use(express.json());
