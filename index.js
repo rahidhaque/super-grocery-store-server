f33a3f30a7a8f96a0f899df0c22cb34c41d0d0bf
rahidhaque <ontirahid@yahoo.com>
2022-05-07 20:48:01 +0600

    Server Running

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
 //database connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhriv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -23,7 +39,7 @@ async function run() {
         app.post('/login', async (req, res) => {
             const user = req.body;
             const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: '20d'
+                expiresIn: '1d'
             })
             res.send({ accessToken });
         })
@@ -37,12 +53,18 @@ async function run() {
         })
 
         //load all inventories for specific user
-        app.get('/myinventory', async (req, res) => {
+        app.get('/myinventory', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = { email: email };
-            const cursor = inventoryCollection.find(query);
-            const myinventory = await cursor.toArray();
-            res.send(myinventory);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = inventoryCollection.find(query);
+                const myinventory = await cursor.toArray();
+                res.send(myinventory);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden Access' });
+            }
         })
 
 
